b89904aee8ae25441f84a8c96208afbd0ae87baf
iLLMannerSevs <54834910+iLLMannerSevs@users.noreply.github.com>
2025-04-24 14:03:10 -0700

    Tool quantity depletion upon crafting
    add logic for checking against tool type used when crafting:
    - if tool has quantity, deplete upon use;
    - if quant reaches 0 -- DeleteSafe

--- IAT/Core/IAT_CraftingPlus/scripts/4_world/actions/iat_actioncraftonworkbench.c
+++ IAT/Core/IAT_CraftingPlus/scripts/4_world/actions/iat_actioncraftonworkbench.c
@@ -69,6 +69,22 @@ class IAT_ActionCraftOnWorkbench extends ActionContinuousBase
       // because players are fucking huge ass babies
 			// craftingWorkbench.DecreaseHealth( craftingWorkbench.GetCraftingDamage(), false );
       // Print(string.Format("Creating %1 from inded %2",newItem.GetDisplayName(), variantId));
+
+
+      // check against tool type (if has quant) & deplete tool quant each time when crafting
+      ItemBase tool = action_data.m_MainItem;
+      if (tool && tool.HasQuantity())
+      {
+        int quantityToReduce = 1; // adjust as needed
+        tool.AddQuantity(-quantityToReduce);
+
+        // deleting tool if quant reaches zero
+        if (tool.GetQuantity() <= 0)
+        {
+          tool.DeleteSafe();
+        }
+      }
+	    
       Object newObject = GetGame().CreateObjectEx(newItem.GetItemClassName(), craftingWorkbench.GetMemoryPointPosition("item_spawn_position"), ECE_SETUP|ECE_NOSURFACEALIGN|ECE_KEEPHEIGHT|ECE_NOLIFETIME|ECE_DYNAMIC_PERSISTENCY);
       if (newItem.GetItemQuantity() > 1)
       {
